8eab5e01178629e526838ec6f92d43142196bf7f
ᴧɴσиʏᴍσᴜs ꭙ˼ <100543828+Miten001@users.noreply.github.com>
2026-09-14 13:28:44 +0530

    Delete assets/js/supabase-config.js

--- assets/js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.FF_SUPABASE_CONFIG = {
-  url: "https://ekrqtlbwaotgdtrpqhrc.supabase.co/rest/v1/",
-  anonKey: "sb_publishable_Q1b_FF9Htl4j30dHashaQQ_8ydq1MU8",
-};
-
-window.FF_ADMIN_EMAILS = ["ravanyt001@gmail.com"];
