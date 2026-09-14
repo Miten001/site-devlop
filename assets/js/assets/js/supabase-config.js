24e0995eabc9434a82b43abcecff001f6a41d1fe
ᴧɴσиʏᴍσᴜs ꭙ˼ <100543828+Miten001@users.noreply.github.com>
2026-09-14 13:27:31 +0530

    Update supabase-config.js

--- assets/js/assets/js/supabase-config.js
+++ assets/js/assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 window.FF_SUPABASE_CONFIG = {
-  url: "https://ekrqtlbwaotgdtrpqhrc.supabase.co/rest/v1/",
+  url: "https://ekrqtlbwaotgdtrpqhrc.supabase.co",
   anonKey: "sb_publishable_Q1b_FF9Htl4j30dHashaQQ_8ydq1MU8",
 };
 
